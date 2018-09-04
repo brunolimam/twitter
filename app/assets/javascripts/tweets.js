c70ebb7ede91fe14dd77d355e751875625b3ab9b
PuckmanXY <kayoanderson0403@gmail.com>
2018-09-04 08:08:15 -0300

    Character counter more efficient

--- app/assets/javascripts/tweets.js
+++ app/assets/javascripts/tweets.js
@@ -1,12 +1,12 @@
-var refreshLength = function () {
+$(document).ready(function() {
   var text = document.getElementById('new_tweet_content');
   var message = document.getElementById('count_message');
   var button = document.getElementById('tweet-button');
   var max_length = 180;
-  var new_length = max_length - text.textLength;
-  var new_message = new_length + "/" + max_length;
-  
-  message.innerHTML = new_message;
-}
 
-setInterval(refreshLength, 10);
+  text.addEventListener("keydown", function(e) {
+    var new_length = max_length - text.textLength;
+    var new_message = new_length + "/" + max_length;
+    message.innerHTML = new_message;
+  })
+})
